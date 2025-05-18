7fb2ed218fa1a077272f366b939d80ca6945500f
Ermir Meziu <ermir.meziu@student.uni-pr.edu>
2025-05-18 16:21:57 +0200

    Added  a logic to send a message to a client

--- src/main/java/ChatClient.java
+++ src/main/java/ChatClient.java
@@ -32,7 +32,7 @@ public class ChatClient {
                     Object response = in.readObject();
                     if (response instanceof HashMap) {
                         publicKeys = (HashMap<String, PublicKey>) response;
-                        System.out.println("🔄 Public Keys Updated: " + publicKeys.keySet());
+                        System.out.println("Public Keys Updated: " + publicKeys.keySet());
                     } else {
                         String encryptedMessage = (String) response;
                         String sender = encryptedMessage.split(": ")[0];
@@ -50,5 +50,24 @@ public class ChatClient {
                 e.printStackTrace();
             }
         }).start();
+
+        while (true) {
+            System.out.println("Choose a recipient: " + publicKeys.keySet());
+            String recipient = scanner.nextLine();
+
+            if (!publicKeys.containsKey(recipient)) {
+                System.out.println("User not found!");
+                continue;
+            }
+
+            System.out.print("Message: ");
+            String message = scanner.nextLine();
+            PublicKey recipientKey = publicKeys.get(recipient);
+
+            String encryptedMessage = rsa.encrypt(message, recipientKey);
+            System.out.println("Encrypted Message: " + encryptedMessage);
+
+            out.writeObject(new String[]{recipient, encryptedMessage});
+        }
     }
 }
