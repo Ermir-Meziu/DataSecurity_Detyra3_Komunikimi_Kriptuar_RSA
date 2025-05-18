d48124e6bf300130d75af7d9a132ec5bde13f34e
kaltrina4 <kaltrine.heta@student.uni-pr.edu>
2025-05-18 15:53:59 +0200

    Shtuar metoda për enkriptim të mesazhit me çelës publik

--- src/main/java/RSAEncryption.java
+++ src/main/java/RSAEncryption.java
@@ -13,4 +13,11 @@ public class RSAEncryption {
     public PublicKey getPublicKey() {
         return keyPair.getPublic();
     }
+
+    public String encrypt(String message, PublicKey publicKey) throws Exception {
+        Cipher cipher = Cipher.getInstance("RSA");
+        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] encryptedBytes = cipher.doFinal(message.getBytes());
+        return java.util.Base64.getEncoder().encodeToString(encryptedBytes);
+    }
 }
